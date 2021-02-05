14953c6db47623c58b1dcb5da6b51a8d9c781de6
Alex <abecker@covenantfrazer.org>
2021-02-05 12:20:28 -0500

    Adding a line so that the react app gets served

--- app.js
+++ app.js
@@ -29,6 +29,10 @@ app.use(compression());
 
 app.use("/auth", authRouter);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
