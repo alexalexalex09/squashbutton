3c74868a84b6c2c722ff4ba0a5a9c25dafe9e010
Alex <abecker@covenantfrazer.org>
2021-02-07 21:20:55 -0500

    Deploy testing

--- app.js
+++ app.js
@@ -122,7 +122,7 @@ app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-
+  console.log(res.url);
   // render the error page
   console.log("Error: " + err);
   res.status(err.status || 500);
