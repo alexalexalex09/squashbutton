e26c8d10db448272b58de51ef6714c1e63ae0aa4
Alex <abecker@covenantfrazer.org>
2021-02-08 21:09:00 -0500

    30 minutes coding, adding css and making sure it deploys correctly to Heroku

--- frontend/src/components/AddNewButton.js
+++ frontend/src/components/AddNewButton.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { FiPlus } from "react-icons/fi";
 
 function AddNewButton() {
   return (
     <div className="addNewButton">
-      <div className="plus">+</div>
+      <div className="plus">
+        <FiPlus></FiPlus>
+      </div>
       <div className="addNewButtonTitle">Add New Button</div>
     </div>
   );
