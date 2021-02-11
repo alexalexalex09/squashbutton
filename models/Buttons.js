244ecc5555cad7bf8e6a0b3952a332a5fab9277e
Alex <abecker@covenantfrazer.org>
2021-02-11 17:52:12 -0500

    Added button-pressed.svg

---
+++ models/Buttons.js
@@ -0,0 +1,31 @@
+//Set up mongoose connection
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+//Create schema
+var ButtonSchema = new Schema(
+  {
+    title: String,
+    history: [String],
+    colors: [{ interval: String, color: String }],
+    preferences: {
+      darkMode: Boolean,
+    },
+  },
+  { collection: "users" }
+);
+
+module.exports = mongoose.model("Button", ButtonSchema);
+/*
+
+//Export function to create "SomeModel" model class
+module.exports = mongoose.model('SomeModel', SomeModelSchema );
+
+You can then require and use the model immediately in other files. Below we show how you might use it to get all instances of the model.
+
+//Create a SomeModel model just by requiring the module
+var SomeModel = require('../models/somemodel')
+
+// Use the SomeModel object (model) to find all SomeModel records
+SomeModel.find(callback_function);
+*/
